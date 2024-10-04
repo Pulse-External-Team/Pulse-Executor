029808faf9d36ee1e550f985a66958b563a53b13
Pulse External Team <jqgsmfang@gmail.com>
2024-10-04 22:53:42 +0200

    Injected.lua

---
+++ injected.lua
@@ -0,0 +1,6 @@
+game.StarterGui:SetCore("SendNotification", {
+    Title = "KRNL remake is Injected";
+    Text = "Made by Pulse External";
+    Icon = "rbxassetid://6023426923"; -- Beispiel für ein Achievement-ähnliches Icon
+    Duration = 2; -- Anzeigezeit in Sekunden
+})
